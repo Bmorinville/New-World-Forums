b4f18f5beffe22f448b6c97a50fd1b69ed5d8905
Bmorinville <70783856+Bmorinville@users.noreply.github.com>
2023-11-13 17:19:06 -0600

    Add files via upload

---
+++ new-world-forums/src/main/java/com/example/newworldforums/NewWorldForumsApplication.java
@@ -0,0 +1,13 @@
+package com.example.newworldforums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWorldForumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWorldForumsApplication.class, args);
+	}
+
+}
---
+++ new-world-forums/src/test/java/com/example/newworldforums/NewWorldForumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newworldforums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWorldForumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
